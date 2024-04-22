29cca4d215456bf97721a2ab75208776a8d3e4e9
Aman Singh Rawat <157161327+aman-git-dec@users.noreply.github.com>
2024-04-23 03:09:31 +0530

    Create level-order-traversal-queue.cpp

---
+++ level-order-traversal-queue.cpp
@@ -0,0 +1,53 @@
+// level order traversal using queue(bfs)
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+  int val;
+  node* left;
+  node* right;
+  node (int val)
+  {
+      this->val=val;
+      this->left=NULL;
+      this->right=NULL;
+  }
+};
+
+void levelorderqueue(node* root)
+{
+    queue<node*> q;
+    q.push(root);
+    while(q.size()>0)
+    {
+        node* temp = q.front();
+        q.pop();
+        cout<<temp->val<<" ";
+        if(temp->left!=NULL) q.push(temp->left);
+          cout<<endl;
+        if(temp->right!=NULL) q.push(temp->right);
+      
+    }
+}
+
+int main() {
+   node* a = new node(1);
+   node* b = new node(2);
+   node* c = new node(3);
+   node* d = new node(4);
+   node* e = new node(5);
+   node* f = new node(6);
+   node* g = new node(7);
+
+   a->left = b;
+   a->right = c;
+   b->left = d;
+   b->right = e;
+   c->left = f;
+   c->right = g;
+
+   levelorderqueue(a);
+
+}
